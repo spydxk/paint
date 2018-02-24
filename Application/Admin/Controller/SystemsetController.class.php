db1f00fdf4c9c52f96b2e399a91e28f53b00cd9f
spydxk <1096022004@qq.com>
2018-02-24 10:31:23 +0800

    提交sys

---
+++ Application/Admin/Controller/SystemsetController.class.php
@@ -0,0 +1,20 @@
+<?php
+/*******************************
+ * 系统设置
+ * @author spy
+ * @company 三级分销系统
+ * @date 2018-2-24
+ ********************************/
+
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class SystemsetController extends PubController
+{
+
+    public function showset()
+    {
+        $this->display();
+    }
+}
